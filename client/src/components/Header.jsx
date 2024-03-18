ecd93bdb0af353d18af90c41a6e19877671e5b75
jayadhav07 <jayadhav2000@gmail.com>
2024-03-18 09:33:40 +0530

    created and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,7 +24,7 @@ const path = useLocation().pathname;
 
   </Button  >
   <Link to='/sign-in'>
-    <Button gradientDuoTone='purpleToBlue'>
+    <Button gradientDuoTone='purpleToBlue' outline>
     Sign In
     </Button>
   </Link>
